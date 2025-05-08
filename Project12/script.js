db7e7da3cea6e90384617c0ef02d219e6cf1663a
nimay003 <edunimay003@gmail.com>
2025-05-08 16:20:38 +0530

    Project 12 Complete

---
+++ Project12/script.js
@@ -0,0 +1,26 @@
+gsap.from("#img1", {
+    deplay:1,
+    opacity:0,
+    duration:1,
+    y:90
+})
+
+gsap.from("#img2", {
+    deplay:1,
+    opacity:0,
+    duration:1,
+    x:90
+})
+
+gsap.from("#img3", {
+    deplay:1,
+    opacity:0,
+    duration:1,
+    y:-90
+})
+
+gsap.from("#main h1", {
+    deplay:0.4,
+    opacity:0,
+    duration:1,
+})
